df32cc22d24129e3140a473bce0a27549fb5f782
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-05-09 19:27:04 +0530

    closures

--- 05_scopes/01_scops.py
+++ 05_scopes/01_scops.py
@@ -21,3 +21,24 @@ def fun3():
 
 # fun3()
 print(x)
+
+"""
+closures 
+aka factory functions
+"""
+def f1():
+    a = 17
+    def f2():
+        print(a)
+    return f2
+
+f1()()
+
+# another ex
+
+def chaicoder(num):
+    def actual(x):
+        return x ** num
+    return actual
+
+print(chaicoder(2)(4))
