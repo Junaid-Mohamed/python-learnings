a47e26076fe9a0672193a4000fb10d0556fe6c48
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-05-10 12:32:07 +0530

    class variables

--- 06_oops/sample.py
+++ 06_oops/sample.py
@@ -3,9 +3,13 @@ class Car:
     # model = None
     # can be done like this aswell but we do it with constructor __init__ method
 
+    # class variables
+    total_cars = 0
     def __init__(self,brand,model):
         self.__brand = brand
         self.model = model
+        self.total_cars += 1 # can be done like this but we can do it directly by accessing the class name as well
+        Car.total_cars += 1
     
     def get_brand(self):
         return self.__brand + " !"
@@ -44,6 +48,9 @@ my_car.brand = "Honda" # set via @brand.setter
 print(my_car.fullName())
 print(my_car.fuel_type())
 
+# access class variable through object or class
+
+print(my_car.total_cars)
 
 class ElectricCar(Car):
 
@@ -58,3 +65,4 @@ myTesla = ElectricCar("Tesla","Model S", "85 KW")
 
 print(myTesla.fullName())
 print(myTesla.fuel_type())
+print(Car.total_cars)
