5ce656cb46a89abc36cb8d0525917f14a8514b21
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-05-10 12:17:12 +0530

    encapsulation

--- 06_oops/sample.py
+++ 06_oops/sample.py
@@ -4,15 +4,42 @@ class Car:
     # can be done like this aswell but we do it with constructor __init__ method
 
     def __init__(self,brand,model):
-        self.brand = brand
+        self.__brand = brand
         self.model = model
     
+    def get_brand(self):
+        return self.__brand + " !"
+
+    def set_brand(self,brand):
+        self.__brand = brand
+    
     def fullName(self):
-        return f"{self.brand} {self.model}"
+        return f"{self.__brand} {self.model}"
+
+    """
+    Using @property and @<property>.setter (Pythonic way):
+    This is cleaner and lets you use attribute-style access while keeping control.
+    """
+
+    @property
+    def brand(self):
+        return self.__brand
+    
+    @brand.setter
+    def brand(self, value):
+        self.__brand = value
 
 my_car = Car("Toyato", "Corolla")
-print(my_car.brand)
+# print(my_car.brand)
 print(my_car.fullName())
+print(my_car.get_brand())
+my_car.set_brand("Maruthi")
+print(my_car.get_brand())
+
+print(my_car.brand) # access via @property
+my_car.brand = "Honda" # set via @brand.setter
+print(my_car.fullName())
+
 
 class ElectricCar(Car):
 
