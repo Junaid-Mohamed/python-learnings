1a4b4af60ff60a9debf1dfd21a6a1723b6fdf9ad
Mohammed Junaid <junaid7mohamed@gmail.com>
2025-05-12 22:52:36 +0530

    debugger - decorator

--- 07_decorators/timer-decorator.py
+++ 07_decorators/timer-decorator.py
@@ -18,4 +18,27 @@ def timer(func):
 def example_function(n):
      time.sleep(n)
     
-example_function(4)
+# example_function(4)
+
+"""
+Problem 2
+Debugging function calls
+decorator to print the function name and the values of its args every time the func is called
+"""
+
+def debugger(func):
+     def wrapper(*args,**kwargs):
+          result = func(*args, **kwargs)
+          print(f"function {func.__name__} is called with following params")
+          if(args):
+               print(args)
+          if(kwargs):
+               print(kwargs)
+          return result
+     return wrapper
+
+@debugger
+def example_function2(a,b, c="junaid"):
+     print(f"this is a function with {a}, {b}, {c} params")
+
+example_function2(2,3,c="Suhail")
